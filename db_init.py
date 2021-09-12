d4045c2eec718df8a845b731dc2f058be0a4a807
SnowLeo819 <jhpak93@gmail.com>
2021-09-13 01:47:18 +0900

    홈트 리스트

--- db_init.py
+++ db_init.py
@@ -10,8 +10,8 @@ db.like.drop();
 # 작성중 내용, 완성본 아님
 
 doc = [
-    {'idx': 1, 'quiz': "코로나 시국 종료!<br>제일 먼저 가고 싶은 여행지는?", 'ans_01': "멋진 야경을 감상할 수 있는<br>뉴욕", 'ans_02': "다양한 액티비티가 가능한<br>스위스",
-     'ans_03': "우아한 만찬과 낭만의 도시<br>프랑스", 'ans_04': "푸른 바다를 보며 휴식할 수 있는<br>괌"},
+    {'idx': 1, 'quiz': "코로나 시국 종료!<br>제일 먼저 가고 싶은 여행지는?", 'ans_01': "야경을 감상할 수 있는<br>뉴욕", 'ans_02': "다양한 액티비티가 가능한<br>스위스",
+     'ans_03': "만찬과 낭만의 도시<br>프랑스", 'ans_04': "바다를 보며 휴식할 수 있는<br>괌"},
     {'idx': 2, 'quiz': "즐거운 여행을 끝마치고<br>집으로 돌아온 나는?", 'ans_01': "여행은 주기적으로 다녀줘야 제 맛,<br>다음엔 어디를 가볼까~", 'ans_02': "친구들에게 여행 썰 풀어줘야지,<br>바로 약속을 잡는다",
      'ans_03': "여행지에서 사온 기념품 정리하고<br>밀린 집안일부터 해야지", 'ans_04': "돌아다니느라 너무 피곤했음,<br>집에서 휴식"},
     {'idx': 3, 'quiz': "갑작스럽게 휴무를 얻게 되었다!<br>오늘의 일정은?", 'ans_01': "다들 모여! 나들이 가자~", 'ans_02': "근손실 못참지!",
@@ -40,10 +40,7 @@ ansDoc = [
     # 'suggest2': '', 'img-link2': '', 'title2': '', 'genre2': '', 'info2':'', 'director2':'', 'link2': ''
     # 'suggest3': '', 'img-link3': '', 'title3': '', 'genre3': '', 'info3':'', 'director3':'', 'link3': ''},
 
-    {'idx': 4, 'type': 'D',
-    'suggest1': '', 'img-link1': 'https://bookthumb-phinf.pstatic.net/cover/206/535/20653532.jpg?type=m1&udate=20210912', 'title1': '오케팅', 'genre1': '특별하지 않아도 누구나 5% 부자가 되는 전략', 'info1':'대한출판사', 'director1':'오두환', 'link1': 'https://book.naver.com/bookdb/book_detail.nhn?bid=20653532',
-    'suggest2': '', 'img-link2': 'https://bookthumb-phinf.pstatic.net/cover/196/181/19618143.jpg?type=m1&udate=20210912', 'title2': '소크라테스 익스프레스', 'genre2': '철학이 우리 인생에 스며드는 순간', 'info2':'어크로스', 'director2':'에릭 와이너', 'link2': 'https://book.naver.com/bookdb/book_detail.nhn?bid=19618143',
-    'suggest3': '', 'img-link3': 'https://bookthumb-phinf.pstatic.net/cover/207/771/20777131.jpg?type=m1&udate=20210912', 'title3': '달러구트 꿈 백화점2', 'genre3': '단골손님을 찾습니다', 'info3':'팩토리나인', 'director3':'이미예', 'link3': 'http://book.naver.com/bookdb/book_detail.nhn?bid=20777131'},
+
 
 
 
@@ -57,9 +54,19 @@ ansDoc = [
      'desc': '카리브해의 산들바람에서 실려온 듯한 라임향에 톡 쏘는 바질과 향기로운 백리향이 더해져 독특한 조합을 만들어 냅니다.',
      'keyword': ['상큼한', '신선한', '밝은', '시트러스']},
 
-    {'idx': 4, 'type': 'D', 'brand': 'BYREDO', 'eng': 'LA TULIPE', 'kor': '바이레도 라튤립',
-     'desc': '한 계절에 처음 맺는 꽃봉오리처럼 활기 넘치고 매력적이고 낙천적인 느낌의 향수',
-     'keyword': ['발랄한', '매력적인', '낙천적인', '플로럴']},
+    {'idx': 4, 'type': 'D',
+     'suggest1': '',
+     'img-link1': 'https://bookthumb-phinf.pstatic.net/cover/206/535/20653532.jpg?type=m1&udate=20210912',
+     'title1': '오케팅', 'genre1': '특별하지 않아도 누구나 5% 부자가 되는 전략', 'info1': '대한출판사', 'director1': '오두환',
+     'link1': 'https://book.naver.com/bookdb/book_detail.nhn?bid=20653532',
+     'suggest2': '',
+     'img-link2': 'https://bookthumb-phinf.pstatic.net/cover/196/181/19618143.jpg?type=m1&udate=20210912',
+     'title2': '소크라테스 익스프레스', 'genre2': '철학이 우리 인생에 스며드는 순간', 'info2': '어크로스', 'director2': '에릭 와이너',
+     'link2': 'https://book.naver.com/bookdb/book_detail.nhn?bid=19618143',
+     'suggest3': '',
+     'img-link3': 'https://bookthumb-phinf.pstatic.net/cover/207/771/20777131.jpg?type=m1&udate=20210912',
+     'title3': '달러구트 꿈 백화점2', 'genre3': '단골손님을 찾습니다', 'info3': '팩토리나인', 'director3': '이미예',
+     'link3': 'http://book.naver.com/bookdb/book_detail.nhn?bid=20777131'},
 
     {'idx': 5, 'type': 'F', 'brand': 'BYREDO', 'eng': 'LA TULIPE', 'kor': '바이레도 라튤립',
      'desc': '한 계절에 처음 맺는 꽃봉오리처럼 활기 넘치고 매력적이고 낙천적인 느낌의 향수',

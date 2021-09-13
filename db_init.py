3ad70cb1564e929d5b49a9e68647f5a728c141f2
SnowLeo819 <jhpak93@gmail.com>
2021-09-13 10:08:56 +0900

    dbinit 수정

--- db_init.py
+++ db_init.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client.perf_it
+db = client.what_today
 
 db.qna.drop();
 db.ans.drop();
@@ -11,7 +11,7 @@ db.like.drop();
 
 doc = [
     {'idx': 1, 'quiz': "코로나 시국 종료!<br>제일 먼저 가고 싶은 여행지는?", 'ans_01': "야경을 감상할 수 있는<br>뉴욕", 'ans_02': "다양한 액티비티가 가능한<br>스위스",
-     'ans_03': "만찬과 낭만의 도시<br>프랑스", 'ans_04': "바다를 보며 휴식할 수 있는<br>괌"},
+     'ans_03': "우아한 만찬과 낭만의 도시<br>파리", 'ans_04': "바다를 보며 휴식할 수 있는<br>괌"},
     {'idx': 2, 'quiz': "즐거운 여행을 끝마치고<br>집으로 돌아온 나는?", 'ans_01': "여행은 주기적으로 다녀줘야 제 맛,<br>다음엔 어디를 가볼까~", 'ans_02': "친구들에게 여행 썰 풀어줘야지,<br>바로 약속을 잡는다",
      'ans_03': "여행지에서 사온 기념품 정리하고<br>밀린 집안일부터 해야지", 'ans_04': "돌아다니느라 너무 피곤했음,<br>집에서 휴식"},
     {'idx': 3, 'quiz': "갑작스럽게 휴무를 얻게 되었다!<br>오늘의 일정은?", 'ans_01': "다들 모여! 나들이 가자~", 'ans_02': "근손실 못참지!",
@@ -47,8 +47,14 @@ ansDoc = [
     {'idx': 1, 'type': 'A', 'suggest': '“ 근손실 방지! 홈트하자~”', 'name': '전신 다이어트 유산소운동 [홈트레이닝]', 'subname': '르라보 어나더 13 EDP',
      'desc': ' 암브록스, 자스민, 이끼, 암브레트 시드 등의 열 세가지 원료들이 섞여 중독성 있는 우디 머스크 계열의 향', 'keyword': ['포근한', '편안한', '성숙한']},
 
-    {'idx': 2, 'type': 'B', 'brand': 'diptyque', 'eng': 'TAMDAO', 'kor': '딥디크 탐다오',
-     'desc': '열대 다우림 속의 나무와 오래된 사원 주위에서 샌덜우드 나무를 끌고 다니는 코끼리 모습을 그린 관능적인 향수', 'keyword': ['관능적인', '매혹적인', '고귀한', '우디']},
+    {'idx': 2, 'type': 'B', 'img-link1':'https://i.ytimg.com/vi/mMxDrgF3yBY/hqdefault.jpg', 'title1':'[운동브이로그] 집순이의 홈트레이닝, 간장계란밥 해먹기, 운동하는 여자🍑',
+     'genre1':'안녕하세요 여러분 디제이 아리에요 영상 많이 시청해주셔서 너무감사합니다!! 운동영상은 12분 부터!!!', 'info1':'2021-05-03T12:43:17Z', 'director1':'삐아리 BBI Ari', 'link1':'mMxDrgF3yBY',
+     'img-link2':'https://i.ytimg.com/vi/_buwjp1ogAQ/hqdefault.jpg', 'title2':'3개월 맨몸 운동 홈트레이닝 루틴 설명',
+     'genre2':'맨몸운동루틴 #홈트레이닝루틴 #맨몸운동변화 맨몸 운동 홈트레이닝으로 3개월 동안 진행한 운동 루틴 질문이 정말 많았어요. 그래서 제가 진행한 운동 루틴을 ...',
+     'info2':'2021-01-30T02:00:04Z', 'director2':'습관의힘제이건', 'link2':'_buwjp1ogAQ',
+     'img-link3':'https://i.ytimg.com/vi/myNjmnvI6x0/hqdefault.jpg', 'title3':'NO 층간소음 올인원 운동 - 40분 유산소운동 홈트 - 관절에 무리없이 체지방 태우기',
+     'genre3':'NO 층간소음 NO 스쿼트 - 40분 유산소운동 홈트 - 관절에 무리없이 체지방 태우기] - 땀 많이 나고 숨이 많이 차는 올인원 운동입니다. NO 층간소음 30동작 유산소 ...',
+     'info3':'2021-01-22T11:39:03Z', 'director3':'빅씨스', 'link3':'myNjmnvI6x0'},
 
     {'idx': 3, 'type': 'C', 'brand': 'Jo Malon', 'eng': 'Lime Basil & Mandarin Cologne', 'kor': '조말론 라임바질앤 만다린',
      'desc': '카리브해의 산들바람에서 실려온 듯한 라임향에 톡 쏘는 바질과 향기로운 백리향이 더해져 독특한 조합을 만들어 냅니다.',
@@ -74,7 +80,7 @@ ansDoc = [
 ]
 
 likeDoc = [
-    {'idx': 1, 'like': 0},
+    {'idx': 1, 'name':'like', 'count': 0},
 ]
 
 

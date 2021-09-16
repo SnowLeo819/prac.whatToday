eaaefc226ea5e71d269a76f1d8ee05dd1ce4c658
SnowLeo819 <jhpak93@gmail.com>
2021-09-16 18:04:20 +0900

    자바오류수정

--- app.py
+++ app.py
@@ -14,30 +14,24 @@ db = client.what_today
 def home():
     return render_template('main.html')
 
-
 @app.route('/Question', methods=['GET'])
 def Question():
     return render_template('question.html')
-
 
 @app.route('/answer', methods=['GET'])
 def result():
     return render_template('answer.html')
 
-
 @app.route('/aboutus', methods=['GET'])
 def aboutus():
     return render_template('aboutus.html')
 
-
 # =======================
-#  좋아요 기능 구현하기
 # 좋아요 가져오기
 @app.route('/like', methods=['GET'])
 def showLike():
     likeCount = list(db.like.find({}, {'_id': False}))
     return jsonify({'likedb': likeCount})
-
 
 # 좋아요 수정 +1
 @app.route('/like/+', methods=['POST'])
@@ -49,7 +43,6 @@ def likePlus():
     db.like.update_one({'name': targetName}, {'$set': {'count': newLike}})
     return jsonify({'msg': '좋아요 완료!'})
 
-
 # 좋아요 수정 -1
 @app.route('/like/-', methods=['POST'])
 def likeMinus():
@@ -60,9 +53,7 @@ def likeMinus():
     db.like.update_one({'name': targetName}, {'$set': {'count': newLike}})
     return jsonify({'msg': '좋아요 취소ㅠㅠ'})
 
-
 # =======================
-
 # 퀴즈정보가 있으면 findDB로 연결(idx 번호롤 판단) / 다음 퀴즈 없는경우(idx번호 없을 때) 실패 반환
 @app.route('/quiz', methods=['GET'])
 def getQuiz():
@@ -72,13 +63,10 @@ def getQuiz():
         return jsonify({'result': 'success', 'quiz': data})
     return jsonify({'result': 'fail'})
 
-
 # DB에서 퀴즈 정보 가져오기
 def findDB(idx):
     data = list(db.qna.find({"idx": int(idx)}, {'_id': False}))
     return data
-
-
 
 # 결과지 가져오기
 @app.route('/ans', methods=['GET'])
@@ -89,13 +77,10 @@ def getAns():
         return jsonify({'result': 'success', 'ans': data})
     return jsonify({'result': 'fail'})
 
-
 # DB에서 결과값 찾기
 def findAnsDB(type):
     data = list(db.ans.find({"type": type}, {'_id': False}))
     return data
 
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=true
-        )
+    app.run('0.0.0.0', port=5000, debug=True)
